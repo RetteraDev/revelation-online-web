91f6d6ee8313b6ed2264fccbe4a2a6b7dbf3d66a
Rettera <trof1m00@mail.ru>
2026-03-07 19:33:25 +0300

    fix: Separate item card header

--- client/src/components/moba/items/ItemCard.tsx
+++ client/src/components/moba/items/ItemCard.tsx
@@ -3,6 +3,7 @@
 import { getItem } from "@/data/moba/items/all";
 import { MobaItemColor, MobaItemStatType, type MobaItem } from "@/data/moba/items/values"
 import { Link } from "@tanstack/react-router";
+import { ItemCardHeader } from "./ItemCardHeader";
 
 interface ItemPreviewProps {
   item: MobaItem;
@@ -22,28 +23,7 @@ const itemBgStyles = {
 function ItemCard({ item }: ItemPreviewProps) {
     return (
         <div className='w-full max-w-sm bg-[rgb(32,30,28)]'>
-            <div 
-                className="flex flex-row items-center gap-3 px-3 relative"
-                style={{ 
-                  backgroundImage: `linear-gradient(0.25turn, ${itemBgStyles[item.color]}, rgb(68, 66, 66)), url('/moba/items/item-bg.jpeg')`,
-                  backgroundSize: 'cover',
-                  backgroundBlendMode: 'overlay',
-                }}
-            >
-                <div className="flex justify-center items-center w-12 h-12">
-                    <img src={item.icon} alt={item.name} className="w-10 h-10 object-cover"/>
-                </div>
-        
-                <span
-                  className={`grow text-gray-100 text-shadow-lg font-medium`}
-                  style={{ WebkitTextStroke: '0.3px #000000' }}
-                >{item.name}</span>
-
-                <div className="flex justify-center items-center gap-1">
-                    <img src={'/moba/items/gold-small.webp'} alt="gold" className="w-4 h-4 object-cover"/>
-                    <span className="text-yellow-600">{item.buyPrice}</span>
-                </div>
-            </div>
+            <ItemCardHeader item={item}/>
             
             <div className="flex flex-col p-3 gap-3">
                 <div className="flex flex-col gap-1">
--- client/src/components/moba/items/ItemListItem.tsx
+++ client/src/components/moba/items/ItemListItem.tsx
@@ -1,15 +1,9 @@
-import { type MobaItem, MobaItemColor } from "@/data/moba/items/values"
+import { type MobaItem } from "@/data/moba/items/values"
 import { Link } from "@tanstack/react-router"
+import { ItemCardHeader } from "./ItemCardHeader"
 
 interface ItemListItemProps {
   item: MobaItem
-  onClick?: (item: MobaItem) => void
-}
-
-const itemBgStyles = {
-  [MobaItemColor.WHITE]: 'rgb(198, 202, 200)',
-  [MobaItemColor.GREEN]: 'rgb(128, 204, 99)',
-  [MobaItemColor.EXCLUSIVE]: 'rgb(139, 99, 204)',
 }
 
 function ItemListItem({ item }: ItemListItemProps) {
@@ -20,28 +14,7 @@ function ItemListItem({ item }: ItemListItemProps) {
         params={{ itemId: item.key }}
         className="block"
       >
-        <div
-          className="flex flex-row items-center gap-3 px-3"
-          style={{ 
-            backgroundImage: `linear-gradient(0.25turn, ${itemBgStyles[item.color]}, rgb(68, 66, 66)), url('/moba/items/item-bg.jpeg')`,
-            backgroundSize: 'cover',
-            backgroundBlendMode: 'overlay',
-          }}
-        >
-          <div className="flex justify-center items-center w-12 h-12">
-            <img src={item.icon} alt={item.name} className="w-10 h-10 object-cover"/>
-          </div>
-
-          <span
-            className={`grow text-gray-100 text-shadow-lg font-medium`}
-            style={{ WebkitTextStroke: '0.3px #000000' }}
-          >{item.name}</span>
-
-          <div className="flex justify-center items-center gap-1">
-            <img src={'/moba/items/gold-small.webp'} alt="gold" className="w-4 h-4 object-cover"/>
-            <span className="text-yellow-600">{item.buyPrice}</span>
-          </div>
-        </div>
+        <ItemCardHeader item={item}/>
       </Link>
   )
 }
